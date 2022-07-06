6298472f90af1cddcc512925b21afcf7e5d59921
Felix Horger <felix.horger@kcl.ac.uk>
2022-07-06 11:34:36 +0100

    Adjusted radial trajectory generation (k=0 problem and half index shift... still a mystery, joking)

--- src/radial.jl
+++ src/radial.jl
@@ -1,10 +1,11 @@
 
 """
-	Assumes the centre sample (or first sample of positive frequencies) is at k = 0
+	Assumes the samples are symmetrically arranged around k = 0,
+	i.e. if num_r is even, no sample hits k = 0, if num_r is odd, the centre one does.
 
 """
 function radial_spokes(φ::AbstractVector{<: Real}, num_r::Integer)
-	r = reshape(range(-π, π * (num_r - 2*mod(num_r+1, 2)) / num_r; length=num_r), 1, num_r)
+	r = reshape(range(-π, π; length=num_r), 1, num_r)
 	range(-π, π * (1 - 2*mod(num_r+1, 2) / num_r); length=num_r)
 	# Allocate memory
 	k = Array{Float64, 3}(undef, 2, num_r, length(φ))
