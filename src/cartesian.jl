ec3808958e5eac4901cc14e2febb272b21e8b3c9
Felix Horger <felix.horger@kcl.ac.uk>
2022-09-22 14:23:05 +0100

    Code for sampling dynamic cartesian k-space

--- src/cartesian.jl
+++ src/cartesian.jl
@@ -1,11 +1,24 @@
 
 """
-Sample each point once, spread uniform randomly over the dynamic axis
+Sample each point in k-space exactly once
 """
 function uniform(shape::NTuple{N, Integer}) where N
 	indices = vec(collect(CartesianIndices(shape)))
 	return shuffle!(indices)
 end
+
+"""
+	Every point in k-t domain has the same probability
+"""
+function uniform_dynamic(shape::NTuple{N, Integer}, num_dynamic::Integer, readouts_per_dynamic::Integer) where N
+	single_repeat_indices = CartesianIndices(shape)
+	indices = Vector{CartesianIndex{N}}(undef, readouts_per_dynamic * num_dynamic)
+	for i = 1:num_dynamic
+		indices[i:num_dynamic:end] = StatsBase.sample(single_repeat_indices, readouts_per_dynamic; replace=false)
+	end
+	return indices
+end
+
 
 function poisson_disk()
 	error("Not implemented")
